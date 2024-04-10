c1a7a9c61a8aa58cc087cd0c74c574d9f94abdcc
Nguyen Quan <nguyenquan2k02@gmail.com>
2024-04-10 22:19:38 +0700

    add new project

---
+++ Controllers/DanhMucController.cs
@@ -0,0 +1,20 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class DanhMucController : ApiController
+    {
+        CSDLTestEntities db = new CSDLTestEntities();
+        [HttpGet]
+        public List<DanhMuc> LayDanhMuc()
+        {
+            return db.DanhMucs.ToList();
+        }
+    }
+}
---
+++ Controllers/SanPhamController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.Models;
+namespace API.Controllers
+{
+    public class SanPhamController : ApiController
+    {
+        CSDLTestEntities db = new CSDLTestEntities();
+        [HttpGet]
+        public List<SanPham> LaySP()
+        {
+            return db.SanPhams.ToList();
+        }
+        [HttpGet]
+        public List<SanPham> TimSPTheoMuc(int madm)
+        {
+            return db.SanPhams.Where(x => x.MaDanhMuc == madm).ToList();
+        }
+        [HttpGet]
+        public SanPham TimSPTheoMa(int ma)
+        {
+            return db.SanPhams.FirstOrDefault(x => x.MaSP == ma);
+        }
+        [HttpPost]
+        public bool ThemMoi(int ma, string ten, int gia, int madm)
+        {
+            SanPham sp = db.SanPhams.FirstOrDefault(x => x.MaSP == ma);
+            if (sp == null)
+            {
+                SanPham sp1 = new SanPham();
+                sp1.MaSP = ma;
+                sp1.TenSP = ten;
+                sp1.DonGia = gia;
+                sp1.MaDanhMuc = madm;
+                db.SanPhams.Add(sp1);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        [HttpPut]
+        public bool CapNhat(int ma, string ten, int gia, int madm)
+        {
+            SanPham sp = db.SanPhams.FirstOrDefault(x => x.MaSP == ma);
+            if (sp != null)
+            {
+                sp.TenSP = ten;
+                sp.DonGia = gia;
+                sp.MaDanhMuc = madm;
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        [HttpDelete]
+        public bool Xoa(int ma)
+        {
+            SanPham sp = db.SanPhams.FirstOrDefault(x => x.MaSP == ma);
+            if (sp != null)
+            {
+                db.SanPhams.Remove(sp);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
